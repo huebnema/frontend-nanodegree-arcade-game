b1cd4c1259b21296414dc6c84e9f613da5a287ea
Maddie <maddie.huebner@gmail.com>
2015-08-25 18:16:50 -0500

    Add strict and allEnemies

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
+"use strict";
+
 // Enemies our player must avoid
 var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+
 };
 
 // Update the enemy's position, required method for game
@@ -28,9 +30,8 @@ Enemy.prototype.render = function() {
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
+var allEnemies = [];
 // Place the player object in a variable called player
-
-
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
