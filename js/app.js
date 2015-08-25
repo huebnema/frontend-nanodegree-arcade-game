8a9f4a8caa585452d6fdbd5cd86842e6e9f3bdab
Maddie <maddie.huebner@gmail.com>
2015-08-25 18:52:57 -0500

    Replace code with help from Udacity Coach Jonathan. Add update and render for Player.

--- js/app.js
+++ js/app.js
@@ -1,13 +1,11 @@
-"use strict";
-
 // Enemies our player must avoid
 var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
+
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-
 };
 
 // Update the enemy's position, required method for game
@@ -26,12 +24,27 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function(){
 
+    this.sprite = 'images/char-boy.png';
+
+};
+
+Player.prototype.update = function(dt) {
+
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
-var allEnemies = [];
 // Place the player object in a variable called player
+
+
+var allEnemies = [];
+var player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -45,3 +58,5 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+
