d097eeb20caae5faa5328ac1a9ee9fe0dbe0646b
Maddie <maddie.huebner@gmail.com>
2015-08-26 18:21:11 -0500

    Simplify player code Add first enemy!

--- js/app.js
+++ js/app.js
@@ -1,13 +1,15 @@
 "use strict";
 
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = x;
+    this.y = y;
 };
 
 // Update the enemy's position, required method for game
@@ -26,9 +28,11 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function(){
+var Player = function(x, y){
 
     this.sprite = 'images/char-boy.png';
+    this.x = x;
+    this.y = y;
 
 };
 
@@ -39,8 +43,6 @@ Player.prototype.update = function(dt) {
 // Troubleshooted using https://discussions.udacity.com/t/opening-the-index-html-is-not-loading-the-game/26674/5
 
 Player.prototype.render = function() {
-    this.x = 200;
-    this.y = 375;
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
@@ -50,9 +52,9 @@ Player.prototype.render = function() {
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-
-var allEnemies = [];
-var player = new Player();
+var enemy1 = new Enemy(100,100);
+var allEnemies = [enemy1];
+var player = new Player(200, 375);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
